cac0b6a3ca0c79f85a1d671832b5c16fd6d70681
STORMTR00P3R <noorblum16@gmail.com>
2023-07-14 17:43:38 -0700

    Update search_spotify

--- get_access_token.sh
+++ get_access_token.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
+CLIENT_ID=""
+CLIENT_SECRET=""
+
 access_refresh=$(curl -X POST "https://accounts.spotify.com/api/token" \
      -H "Content-Type: application/x-www-form-urlencoded" \
-     -d "grant_type=client_credentials&client_id=09d8435e360d438aad8f71c48044d7fe&client_secret=a983af0557044085a141e7f5a44ac723")
+     -d "grant_type=client_credentials&client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET")
 
 access_token=$(echo "$access_refresh" | jq -r '.access_token')
 
-echo "$access_token" > "./access_token"
-
+echo -n "$access_token" > "./access_token"
